c669098eefbabd818280003baa82739d9652220a
Arun Gound <arun00gound@gmail.com>
2023-09-28 01:26:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Body />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <HeaderButton title="Open" />
+      <HeaderButton title="Close" />
+      <HeaderButton title="Boost" />
+    </div>
+  );
+}
+
+function HeaderButton({ title }) {
+  return <button className="header-btn">{title}</button>;
+}
+
+function Body() {
+  return (
+    <div className="body">
+      <Form />
+      <Placeholder />
+      <Placeholder />
+    </div>
+  );
+}
+
+function Form() {
+  return (
+    <div className="form">
+      <form action="" method="GET">
+        <label for="asset-type">Select asset</label>
+        <select name="asset" id="asset-type">
+          <option value="eth">ETH</option>
+        </select>
+        <label for="borrow-amt">Borrow Amount</label>
+        <input type="tel" id="borrow-amt" name="borrow-amt" />
+        <textarea name="textarea" readOnly></textarea>
+        <button type="submit">Execute</button>
+      </form>
+    </div>
+  );
+}
+
+function Placeholder() {
+  return <div className="placeholder"></div>;
 }
 
 export default App;
